1f798388ba26fc7bed6073539be091501737e635
Reda Maddi <reda9627@gmail.com>
2026-01-06 00:08:45 +0100

    Builder first

---
+++ gestion_location_vehicule/src/main/java/com/example/gestion_location_vehicule/LocationVehiculeProjetG6Application.java
@@ -0,0 +1,13 @@
+package com.example.gestion_location_vehicule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationVehiculeProjetG6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationVehiculeProjetG6Application.class, args);
+	}
+
+}
---
+++ gestion_location_vehicule/src/test/java/com/example/gestion_location_vehicule/LocationVehiculeProjetG6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_location_vehicule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationVehiculeProjetG6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
