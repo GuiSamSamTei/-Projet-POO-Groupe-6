38022667b46c28d461167ba52c2e43f45b178c05
Rodolphe <rgroscaux@gmail.com>
2026-01-06 16:59:37 +0100

    commit

---
+++ src/main/java/com/example/gestion_location_vehicule/model/Entreprise.java
@@ -0,0 +1,31 @@
+package com.example.gestion_location_vehicule.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Entreprise {
+
+    @Id
+    private String Siret;
+
+    private String raisonSoc;
+
+    private String ville;
+
+    private String adresse;
+
+    private String telephone;
+
+    private String email;
+
+    private boolean active;
+}
