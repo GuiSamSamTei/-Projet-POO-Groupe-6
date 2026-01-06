21d04004f40276dd5237cf207d967d78a8eb4cd0
Rodolphe <rgroscaux@gmail.com>
2026-01-06 16:06:03 +0100

    commit

---
+++ src/main/java/com/example/gestion_location_vehicule/model/Vehicule.java
@@ -0,0 +1,39 @@
+package com.example.gestion_location_vehicule.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Vehicule {
+
+
+    @Id
+    private Long id;
+
+    private String marque;
+
+    private String modele;
+
+    private String couleur;
+
+    private double prixJour;
+
+    private int noteVehicule;
+
+    private boolean vehiculeDispo;
+
+    private Date dateDispo;
+
+    private String villeDispo;
+
+}
