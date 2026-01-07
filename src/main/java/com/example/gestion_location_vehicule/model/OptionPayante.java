a316bd1aa38d5389eeb3abab0f73717a6d71ac00
Rodolphe <rgroscaux@gmail.com>
2026-01-07 15:25:40 +0100

    commit

---
+++ src/main/java/com/example/gestion_location_vehicule/model/OptionPayante.java
@@ -0,0 +1,30 @@
+package com.example.gestion_location_vehicule.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OptionPayante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+
+    private String description;
+
+    private double prixMensuel;
+
+    private boolean active;
+}
---
+++ src/main/java/com/example/gestion_location_vehicule/model/Tarification.java
@@ -0,0 +1,28 @@
+package com.example.gestion_location_vehicule.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tarification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double prixFixe;       // ex : 2€ par jour
+
+    private double pourcentage;  // ex : 10% de commission
+
+    private long annee;
+}
