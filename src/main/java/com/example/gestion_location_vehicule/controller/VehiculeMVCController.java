f1b07a48ad6463928e3b723005188cc54515cf57
Rodolphe <rgroscaux@gmail.com>
2026-01-08 17:14:48 +0100

    commit repository

---
+++ src/main/java/com/example/gestion_location_vehicule/controller/VehiculeMVCController.java
@@ -0,0 +1,29 @@
+package com.example.gestion_location_vehicule.controller;
+
+import com.example.gestion_location_vehicule.model.Vehicule;
+import com.example.gestion_location_vehicule.service.VehiculeService.VehiculeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/vehicules")
+public class VehiculeMVCController {
+
+    private final VehiculeService vehiculeService;
+
+    public VehiculeMVCController(VehiculeService vehiculeService) {
+        this.vehiculeService = vehiculeService;
+    }
+
+    @GetMapping("/liste")
+    public String showVehicules(Model model) {
+        // Récupère tous les véhicules disponibles
+        List<Vehicule> vehicules = vehiculeService.getAllVehicules();
+        model.addAttribute("vehicules", vehicules);
+        return "loueur/vehicule/vehicules"; // Correspond à src/main/resources/templates/vehicule/vehicules.html
+    }
+}
