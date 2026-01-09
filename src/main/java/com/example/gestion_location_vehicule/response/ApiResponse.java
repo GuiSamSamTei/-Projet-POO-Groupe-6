7252319a23492cc0a8ed0d9ce933a62b09d447c0
Reda Maddi <reda9627@gmail.com>
2026-01-09 10:42:36 +0100

    totu

---
+++ src/main/java/com/example/gestion_location_vehicule/response/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.example.gestion_location_vehicule.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+
+    private String message;
+    private Object data;
+}
