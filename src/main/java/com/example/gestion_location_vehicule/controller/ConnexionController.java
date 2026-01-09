b3505487dfd9d07e46af3a24ea4dceb8acc198f1
Rodolphe <rgroscaux@gmail.com>
2026-01-09 16:16:03 +0100

    commit repository

--- src/main/java/com/example/gestion_location_vehicule/controller/ConnexionController.java
+++ src/main/java/com/example/gestion_location_vehicule/controller/ConnexionController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/connexion")
 public class ConnexionController {
 
     private final IUtilisateurService utilisateurService;
